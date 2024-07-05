99e122587104a584c62b62f38e2c9555e6383175
msaleheen12 <msaleheen@ivytech.edu>
2024-07-06 00:35:27 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+from fractions import Fraction
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+#The number of tests conducted and their pass/fail status are displayed in the test results. "OK" indicates that the test run was successful. You will get a traceback that details the failure's location and kind if any tests fail.
